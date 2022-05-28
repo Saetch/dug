b5ab80833729c9dc0189009b98baca315cae64c1
Saetch <nils.wriedt@hotmail.com>
2022-05-28 21:59:47 +0200

    added some logic for runtime texture arrays

--- src/rendering.rs
+++ src/rendering.rs
@@ -17,7 +17,7 @@
 // what a vertex or a shader is.
 
 use bytemuck::{Pod, Zeroable};
-use std::{sync::{Arc, RwLock, atomic::AtomicBool}, thread::JoinHandle, time::{SystemTime, Duration}};
+use std::{sync::{Arc, RwLock, atomic::AtomicBool}, thread::JoinHandle, time::{SystemTime, Duration}, io::Cursor};
 use vulkano::{
     buffer::{BufferUsage, CpuAccessibleBuffer, TypedBufferAccess},
     command_buffer::{
@@ -27,22 +27,22 @@ use vulkano::{
         physical::{PhysicalDevice, PhysicalDeviceType},
         Device, DeviceCreateInfo, DeviceExtensions, QueueCreateInfo, Features,
     },
-    image::{view::ImageView, ImageAccess, ImageUsage, SwapchainImage},
+    image::{view::ImageView, ImageAccess, ImageUsage, SwapchainImage, ImageDimensions, ImmutableImage, MipmapsCount},
     impl_vertex,
     instance::{Instance, InstanceCreateInfo},
     pipeline::{
         graphics::{
             input_assembly::InputAssemblyState,
             vertex_input::BuffersDefinition,
-            viewport::{Viewport, ViewportState},
+            viewport::{Viewport, ViewportState}, color_blend::ColorBlendState,
         },
-        GraphicsPipeline,
+        GraphicsPipeline, layout::PipelineLayoutCreateInfo, PipelineLayout, Pipeline, PipelineBindPoint,
     },
     render_pass::{Framebuffer, FramebufferCreateInfo, RenderPass, Subpass},
     swapchain::{
         acquire_next_image, AcquireError, Swapchain, SwapchainCreateInfo, SwapchainCreationError,
     },
-    sync::{self, FlushError, GpuFuture},
+    sync::{self, FlushError, GpuFuture}, format::Format, sampler::{Sampler, SamplerCreateInfo, SamplerAddressMode, Filter}, descriptor_set::{layout::{DescriptorSetLayoutCreateInfo, DescriptorSetLayout, DescriptorSetLayoutCreationError}, PersistentDescriptorSet, WriteDescriptorSet},
 };
 use vulkano_win::VkSurfaceBuild;
 use winit::{
@@ -370,14 +370,20 @@ pub(crate) fn vulkano_render(mut threads_vec : Vec<JoinHandle<()>>, running : Ar
         vulkano_shaders::shader! {
             ty: "vertex",
             src: "
-				#version 450
-
-				layout(location = 0) in vec2 position;
-
-				void main() {
-					gl_Position = vec4(position, 0.0, 1.0);
-				}
-			"
+            #version 450
+            
+            layout(location = 0) in vec2 position;
+            layout(location = 1) in uint tex_i;
+            layout(location = 2) in vec2 coords;
+            
+            layout(location = 0) out flat uint out_tex_i;
+            layout(location = 1) out vec2 out_coords;
+            
+            void main() {
+                gl_Position = vec4(position, 0.0, 1.0);
+                out_tex_i = tex_i;
+                out_coords = coords;
+            }"
         }
     }
 
@@ -385,14 +391,20 @@ pub(crate) fn vulkano_render(mut threads_vec : Vec<JoinHandle<()>>, running : Ar
         vulkano_shaders::shader! {
             ty: "fragment",
             src: "
-				#version 450
-
-				layout(location = 0) out vec4 f_color;
-
-				void main() {
-					f_color = vec4(1.0, 0.0, 0.0, 1.0);
-				}
-			"
+            #version 450
+            
+            #extension GL_EXT_nonuniform_qualifier : enable
+            
+            layout(location = 0) in flat uint tex_i;
+            layout(location = 1) in vec2 coords;
+            
+            layout(location = 0) out vec4 f_color;
+            
+            layout(set = 0, binding = 0) uniform sampler2D tex[];
+            
+            void main() {
+                f_color = texture(tex[tex_i], coords);
+            }"
         }
     }
 
@@ -435,6 +447,177 @@ pub(crate) fn vulkano_render(mut threads_vec : Vec<JoinHandle<()>>, running : Ar
     )
     .unwrap();
 
+
+
+
+    let mascot_texture = {
+        let png_bytes = include_bytes!("rust_mascot.png").to_vec();
+        let cursor = Cursor::new(png_bytes);
+        let decoder = png::Decoder::new(cursor);
+        let mut reader = decoder.read_info().unwrap();
+        let info = reader.info();
+        let dimensions = ImageDimensions::Dim2d {
+            width: info.width,
+            height: info.height,
+            array_layers: 1,
+        };
+        let mut image_data = Vec::new();
+        image_data.resize((info.width * info.height * 4) as usize, 0);
+        reader.next_frame(&mut image_data).unwrap();
+
+        let image = ImmutableImage::from_iter(
+            image_data,
+            dimensions,
+            MipmapsCount::One,
+            Format::R8G8B8A8_SRGB,
+            queue.clone(),
+        )
+        .unwrap()
+        .0;
+
+        ImageView::new_default(image).unwrap()
+    };
+
+    let vulkano_texture = {
+        let png_bytes = include_bytes!("vulkano_logo.png").to_vec();
+        let cursor = Cursor::new(png_bytes);
+        let decoder = png::Decoder::new(cursor);
+        let mut reader = decoder.read_info().unwrap();
+        let info = reader.info();
+        let dimensions = ImageDimensions::Dim2d {
+            width: info.width,
+            height: info.height,
+            array_layers: 1,
+        };
+        let mut image_data = Vec::new();
+        image_data.resize((info.width * info.height * 4) as usize, 0);
+        reader.next_frame(&mut image_data).unwrap();
+
+        let image = ImmutableImage::from_iter(
+            image_data,
+            dimensions,
+            MipmapsCount::One,
+            Format::R8G8B8A8_SRGB,
+            queue.clone(),
+        )
+        .unwrap()
+        .0;
+
+        ImageView::new_default(image).unwrap()
+    };
+
+
+
+
+
+
+
+    let Dwarf_Base_house_texture = {
+        let png_bytes = include_bytes!("../Dwarf_BaseHouse.png").to_vec();
+        let cursor = Cursor::new(png_bytes);
+        let decoder = png::Decoder::new(cursor);
+        let mut reader = decoder.read_info().unwrap();
+        let info = reader.info();
+        let dimensions = ImageDimensions::Dim2d {
+            width: info.width,
+            height: info.height,
+            array_layers: 1,
+        };
+        let mut image_data = Vec::new();
+        image_data.resize((info.width * info.height * 4) as usize, 0);
+        reader.next_frame(&mut image_data).unwrap();        //putting the start into the image_data vector, which is also an iterator
+
+        let image = ImmutableImage::from_iter(
+            image_data,
+            dimensions,
+            MipmapsCount::One,
+            Format::R8G8B8A8_SRGB,
+            queue.clone(),
+        )
+        .unwrap()
+        .0;
+
+        ImageView::new_default(image).unwrap()
+    };
+
+    let rust_logo_texture = {
+        let png_bytes = include_bytes!("../image_img.png").to_vec();
+        let cursor = Cursor::new(png_bytes);
+        let decoder = png::Decoder::new(cursor);
+        let mut reader = decoder.read_info().unwrap();
+        let info = reader.info();
+        let dimensions = ImageDimensions::Dim2d {
+            width: info.width,
+            height: info.height,
+            array_layers: 1,
+        };
+        let mut image_data = Vec::new();
+        image_data.resize((info.width * info.height * 4) as usize, 0);
+        reader.next_frame(&mut image_data).unwrap();
+
+        let image = ImmutableImage::from_iter(
+            image_data,
+            dimensions,
+            MipmapsCount::One,
+            Format::R8G8B8A8_SRGB,
+            queue.clone(),
+        )
+        .unwrap()
+        .0;
+
+        ImageView::new_default(image).unwrap()
+    };
+
+    //A sampler is an object that gives the color value of an texture for the given coordinates
+    let sampler = Sampler::new(
+        device.clone(),
+        SamplerCreateInfo {
+            mag_filter: Filter::Linear,
+            min_filter: Filter::Linear,
+            address_mode: [SamplerAddressMode::Repeat; 3],
+            ..Default::default()
+        },
+    )
+    .unwrap();
+
+
+    //create a pipeline layout that can take the bindings for the texture samplers
+    let pipeline_layout = {
+        let mut layout_create_infos: Vec<_> = DescriptorSetLayoutCreateInfo::from_requirements(
+            fs.entry_point("main").unwrap().descriptor_requirements(),
+        );
+
+        // Set 0, Binding 0
+        let binding = layout_create_infos[0].bindings.get_mut(&0).unwrap();
+        binding.variable_descriptor_count = true;
+        binding.descriptor_count = 2;
+
+        let set_layouts = layout_create_infos
+            .into_iter()
+            .map(|desc| Ok(DescriptorSetLayout::new(device.clone(), desc.clone())?))
+            .collect::<Result<Vec<_>, DescriptorSetLayoutCreationError>>()
+            .unwrap();
+
+        PipelineLayout::new(
+            device.clone(),
+            PipelineLayoutCreateInfo {
+                set_layouts,
+                push_constant_ranges: fs
+                    .entry_point("main")
+                    .unwrap()
+                    .push_constant_requirements()
+                    .cloned()
+                    .into_iter()
+                    .collect(),
+                ..Default::default()
+            },
+        )
+        .unwrap()
+    };
+
+
+    //the subpass the pipeline will be used for. This needs to be specified in the pipeline creation and the pipeline will only be usable here
+    let subpass = Subpass::from(render_pass.clone(), 0).unwrap();
     // Before we draw we have to create what is called a pipeline. This is similar to an OpenGL
     // program, but much more specific.
     let pipeline = GraphicsPipeline::start()
@@ -443,18 +626,38 @@ pub(crate) fn vulkano_render(mut threads_vec : Vec<JoinHandle<()>>, running : Ar
         // A Vulkan shader can in theory contain multiple entry points, so we have to specify
         // which one.
         .vertex_shader(vs.entry_point("main").unwrap(), ())
-        // The content of the vertex buffer describes a list of triangles.
-        .input_assembly_state(InputAssemblyState::new())
         // Use a resizable viewport set to draw over the entire window
         .viewport_state(ViewportState::viewport_dynamic_scissor_irrelevant())
         // See `vertex_shader`.
         .fragment_shader(fs.entry_point("main").unwrap(), ())
-        // We have to indicate which subpass of which render pass this pipeline is going to be used
-        // in. The pipeline will only be usable from this particular subpass.
-        .render_pass(Subpass::from(render_pass.clone(), 0).unwrap())
+        .color_blend_state(ColorBlendState::new(subpass.num_color_attachments()).blend_alpha())
+        .render_pass(subpass)
         // Now that our builder is filled, we call `build()` to obtain an actual pipeline.
         .build(device.clone())
         .unwrap();
+
+    let layout = pipeline.layout().set_layouts().get(0).unwrap();
+
+    let descriptors = [
+        (vulkano_texture.clone() as _, sampler.clone()),
+        (mascot_texture.clone() as _, sampler.clone()),
+    ];
+
+    let descriptor_array = [WriteDescriptorSet::image_view_sampler_array(
+        0,
+        0,
+        descriptors
+        ,
+    )];
+
+    //descriptor sets explain to a render pass what objects are accessible
+    let descriptor_set = PersistentDescriptorSet::new_variable(
+        layout.clone(),
+        2,
+        descriptor_array
+        ,
+    )
+    .unwrap();
 
     // Dynamic viewports allow us to recreate just the viewport when the window is resized
     // Otherwise we would have to recreate the whole pipeline.
@@ -621,6 +824,12 @@ pub(crate) fn vulkano_render(mut threads_vec : Vec<JoinHandle<()>>, running : Ar
                     // Since we used an `EmptyPipeline` object, the objects have to be `()`.
                     .set_viewport(0, [viewport.clone()])
                     .bind_pipeline_graphics(pipeline.clone())
+                    .bind_descriptor_sets(
+                        PipelineBindPoint::Graphics,
+                        pipeline.layout().clone(),
+                        0,
+                        descriptor_set.clone(),
+                    )
                     .bind_vertex_buffers(0, vertex_buffer.clone())
                     .draw(vertex_buffer.len() as u32, 1, 0, 0)
                     .unwrap()
@@ -664,7 +873,7 @@ pub(crate) fn vulkano_render(mut threads_vec : Vec<JoinHandle<()>>, running : Ar
             }
             Event::MainEventsCleared => {
                 let now_time = SystemTime::now();
-                if (now_time.duration_since(last_change)).unwrap().as_millis() > 10 {
+                if (now_time.duration_since(last_change)).unwrap().as_millis() > 10 && false {
                     last_change = SystemTime::now();
                     vertices.iter_mut().for_each(|v| {v.position[1]+= 0.0005 });
                     //TODO: This should not be here, the vertex_buffer should be recreated when drawing, not when updating the logic
